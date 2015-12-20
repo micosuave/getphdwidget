a75d3dc34158c44a15b421d1c96d94bbdf997c36
Michael Costa <michaelmcosta@me.com>
2015-12-19 20:20:47 -0500

    jungle

--- src/droppable.js
+++ src/droppable.js
@@ -59,9 +59,62 @@ angular.module("fa.droppable", [])
         };
     }])
 
-    .controller("DropFilesController", ['$controller', 'extract', '$scope',  '$timeout', '$rootScope',
-        function ($controller, extract, $scope, $timeout, $rootScope) {
+    .controller("DropFilesController", ['$controller', 'extract', '$scope',  '$timeout', '$rootScope','FileUploader',
+        function ($controller, extract, $scope, $timeout, $rootScope, FileUploader) {
             var drop = this;
+           var uploader = $scope.uploader = new FileUploader({
+            url: $scope.url || 'https://lexlab.io/upload',
+            autoUpload: true
+        });
+
+        // FILTERS
+
+        uploader.filters.push({
+            name: 'customFilter',
+            fn: function(item /*{File|FileLikeObject}*/, options) {
+                return this.queue.length < 10;
+            }
+        });
+
+        // CALLBACKS
+
+        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+            console.info('onWhenAddingFileFailed', item, filter, options);
+        };
+        uploader.onAfterAddingFile = function(fileItem) {
+            console.info('onAfterAddingFile', fileItem);
+        };
+        uploader.onAfterAddingAll = function(addedFileItems) {
+            console.info('onAfterAddingAll', addedFileItems);
+        };
+        uploader.onBeforeUploadItem = function(item) {
+            console.info('onBeforeUploadItem', item);
+        };
+        uploader.onProgressItem = function(fileItem, progress) {
+            console.info('onProgressItem', fileItem, progress);
+        };
+        uploader.onProgressAll = function(progress) {
+            console.info('onProgressAll', progress);
+        };
+        uploader.onSuccessItem = function(fileItem, response, status, headers) {
+            console.info('onSuccessItem', fileItem, response, status, headers);
+        };
+        uploader.onErrorItem = function(fileItem, response, status, headers) {
+            console.info('onErrorItem', fileItem, response, status, headers);
+        };
+        uploader.onCancelItem = function(fileItem, response, status, headers) {
+            console.info('onCancelItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteItem = function(fileItem, response, status, headers) {
+            console.info('onCompleteItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteAll = function() {
+            console.info('onCompleteAll');
+        };
+
+        console.info('uploader', uploader);
+           
+           
             //var main = $controller('MainCtrl');
             //var main = $scope.main;
             // $scope.log = '';
