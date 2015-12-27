9fc319a18e5017bde04586ea9b1466772b8cedb0
Michael Costa <michaelmcosta@me.com>
2015-12-27 03:11:17 -0500

    wg

--- dist/adf-widget-getphd.js
+++ dist/adf-widget-getphd.js
@@ -320,7 +320,7 @@ angular.module('adf.widget.getphd', ['adf.provider', 'llp.extract',
                 main.error = null;
                 main.success = null;
                toastr.warning('starting extraction...');
-                extractpdf(file)
+                extractpdf(file, config.appnum)
                     .then(function (files) {
                       $log.info('Files extracted', files);
                       alertify.log('Files extracted');
