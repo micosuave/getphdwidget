fa36c418c5e7dfa3b61e86e0517f0e2731b7defa
Michael Costa <micosuave@MICOFF.local>
2016-01-31 21:14:07 -0500

    u

--- dist/adf-widget-getphd.js
+++ dist/adf-widget-getphd.js
@@ -375,39 +375,39 @@ angular.module('adf.widget.getphd', ['adf.provider', 'llp.extract',
       main.size="300px";
       main.checked = false;
       
-      var cors_api_url = 'https://lexlab.io/proxy/';
-  function doCORSRequest(options, printResult) {
-    var x = new XMLHttpRequest();
-    x.open(options.method, cors_api_url + options.url);
-    x.onload = x.onerror = function() {
-      printResult(
-        options.method + ' ' + options.url + '\n' +
-        x.status + ' ' + x.statusText + '\n\n' +
-        (x.responseText || '')
-      );
-    };
-    if (/^POST/i.test(options.method)) {
-      x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    }
-    x.send(options.data);
-  }
-  // Bind event
-  (function() {
-    var urlField = document.getElementById('url');
-    var dataField = document.getElementById('data');
-    var outputField = document.getElementById('output');
-    document.getElementById('get').onclick =
-    document.getElementById('post').onclick = function(e) {
-      e.preventDefault();
-      doCORSRequest({
-        method: this.id === 'post' ? 'POST' : 'GET',
-        url: urlField.value,
-        data: dataField.value
-      }, function printResult(result) {
-        outputField.value = result;
-      });
-    };
-  })();
+//       var cors_api_url = 'https://lexlab.io/proxy/';
+//   function doCORSRequest(options, printResult) {
+//     var x = new XMLHttpRequest();
+//     x.open(options.method, cors_api_url + options.url);
+//     x.onload = x.onerror = function() {
+//       printResult(
+//         options.method + ' ' + options.url + '\n' +
+//         x.status + ' ' + x.statusText + '\n\n' +
+//         (x.responseText || '')
+//       );
+//     };
+//     if (/^POST/i.test(options.method)) {
+//       x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+//     }
+//     x.send(options.data);
+//   }
+//   // Bind event
+//   (function() {
+//     var urlField = document.getElementById('url');
+//     var dataField = document.getElementById('data');
+//     var outputField = document.getElementById('output');
+//     document.getElementById('get').onclick =
+//     document.getElementById('post').onclick = function(e) {
+//       e.preventDefault();
+//       doCORSRequest({
+//         method: this.id === 'post' ? 'POST' : 'GET',
+//         url: urlField.value,
+//         data: dataField.value
+//       }, function printResult(result) {
+//         outputField.value = result;
+//       });
+//     };
+//   })();
       
       
       main.getfilehistory = function (appnum) {
