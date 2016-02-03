6614856dbcbeb80c2526e9767cd1866d0626cb3e
Michael Costa <micosuave@MICOFF.local>
2016-02-03 18:01:41 -0500

    u

--- dist/adf-widget-getphd.js
+++ dist/adf-widget-getphd.js
@@ -1039,7 +1039,7 @@ angular.module('textSizeSlider', [])
                
                  var matter = Matter($stateParams.matterId, $stateParams.groupId);
                  var collections = Collections();
-                 var dashboards = Collection($statParams.page);
+                 var dashboards = Collection($statParams.pageid);
                  var dashboardsref = dashboards.$ref();
                 //  var phdref = Collection(phd.id).$ref();
                  var projref = Collection($stateParams.pId).$ref();
