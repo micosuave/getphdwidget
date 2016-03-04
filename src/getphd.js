3d1d053001293bc04920a33adbfa394fa15ef03a
Michael Costa <michael@mylionlaw.com>
2016-03-04 08:40:53 -0500

    u

--- src/getphd.js
+++ src/getphd.js
@@ -187,7 +187,9 @@ angular.module('adf.widget.getphd', ['adf.provider', 'llp.extract',
       uploader.filters.push({
         name: 'customFilter',
         fn: function (item /*{File|FileLikeObject}*/, options) {
-          return this.queue.length < 10;
+          var filename = item.filename || item.name;
+          
+          return (filename.indexOf('.zip') > -1 && filename.indexOf('(') == -1 && filename.indexOf(' ') == -1);
         }
       });
 
@@ -195,6 +197,7 @@ angular.module('adf.widget.getphd', ['adf.provider', 'llp.extract',
 
       uploader.onWhenAddingFileFailed = function (item /*{File|FileLikeObject}*/, filter, options) {
         console.info('onWhenAddingFileFailed', item, filter, options);
+        alertify.alert('The file\'s name must end with ".zip" and consist only of numbers -  "(",")",",","/", or " " are not permitted.');
       };
       uploader.onAfterAddingFile = function (fileItem) {
         console.info('onAfterAddingFile', fileItem);
@@ -960,4 +963,47 @@ angular.module('adf.widget.getphd', ['adf.provider', 'llp.extract',
       gallery.slides.push({ title: 'PatentPhD', media:'/llp_core/img/logolong.png' });
     }
   }])
-  .controller('GalleryCarousel',['',function(){}]);
+  .controller('GalleryCarousel',['',function(){}]).directive('ngThumb', ['$window', function($window) {
+        var helper = {
+            support: !!($window.FileReader && $window.CanvasRenderingContext2D),
+            isFile: function(item) {
+                return angular.isObject(item) && item instanceof $window.File;
+            },
+            isImage: function(file) {
+                var type =  '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+        };
+
+        return {
+            restrict: 'A',
+            template: '<canvas/>',
+            link: function(scope, element, attributes) {
+                if (!helper.support) return;
+
+                var params = scope.$eval(attributes.ngThumb);
+
+                if (!helper.isFile(params.file)) return;
+                if (!helper.isImage(params.file)) return;
+
+                var canvas = element.find('canvas');
+                var reader = new FileReader();
+
+                reader.onload = onLoadFile;
+                reader.readAsDataURL(params.file);
+
+                function onLoadFile(event) {
+                    var img = new Image();
+                    img.onload = onLoadImage;
+                    img.src = event.target.result;
+                }
+
+                function onLoadImage() {
+                    var width = params.width || this.width / this.height * params.height;
+                    var height = params.height || this.height / this.width * params.width;
+                    canvas.attr({ width: width, height: height });
+                    canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
+                }
+            }
+        };
+    }]);
