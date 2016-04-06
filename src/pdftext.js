415de4c37e799dda3ad4788abe7ca298b6de9325
Michael Costa <michaelmcosta@me.com>
2016-04-05 20:01:11 -0400

    u

--- src/pdftext.js
+++ src/pdftext.js
@@ -50,49 +50,113 @@ angular.module('llp.pdf', ['LocalStorageModule'])
 
 
     }])
-    .directive('getpdftext', ['extract', '$document', '$window', '$rootScope',
-        function(extract, $document, $window, $rootScope) {
+    .directive('getpdftext', ['extract', '$document', '$window', '$rootScope','$http',
+        function(extract, $document, $window, $rootScope, $http) {
             var linkfunction = function($scope, $element, $attr, $ctrl) {
+                $scope.pages = [];
+                
+                $scope.keywords = ['claim\s\d+','claims\s\d+','rejected under', '101','102','103','112','teaches','teach','in view of','limitations','limit','taught'];
+$http.get($attr.pdfData).then(function(resp){
+    
+PDFJS.workerSrc = '/llp_core/bower_components/pdfjs-dist/build/pdf.worker.js';
+var PDF_PATH = $attr.pdfData;
+var PAGE_NUMBER = 1;
+var PAGE_SCALE = 0.25;
+var SVG_NS = 'http://www.w3.org/2000/svg';
 
+function buildSVG(viewport, textContent) {
+  // Building SVG with size of the viewport (for simplicity)
+  var svg = document.createElementNS(SVG_NS, 'svg:svg');
+  //svg.setAttribute('width', viewport.width + 'px');
+  //svg.setAttribute('height', viewport.height + 'px');
+  svg.setAttribute('width', '50vw');
+  svg.setAttribute('height', '50vh');
+  // items are transformed to have 1px font size
+  svg.setAttribute('font-size', 1);
 
+  // processing all items
+  textContent.items.forEach(function (textItem) {
+    // we have to take in account viewport transform, which incudes scale,
+    // rotation and Y-axis flip, and not forgetting to flip text.
+    var tx = PDFJS.Util.transform(
+      PDFJS.Util.transform(viewport.transform, textItem.transform),
+      [1, 0, 0, -1, 0, 0]);
+    var style = textContent.styles[textItem.fontName];
+    // adding text element
+    var text = document.createElementNS(SVG_NS, 'svg:text');
+    text.setAttribute('transform', 'matrix(' + tx.join(' ') + ')');
+    text.setAttribute('font-family', style.fontFamily);
+    text.textContent = textItem.str;
+    svg.appendChild(text);
+  });
+  return svg;
+}
 
+function pageLoaded(PAGE_NUMBER) {
+  // Loading document and page text content
+  PDFJS.getDocument(PDF_PATH).then(function (pdfDocument) {
+    pdfDocument.getPage(PAGE_NUMBER).then(function (page) {
+      var viewport = page.getViewport(PAGE_SCALE);
+      page.getTextContent().then(function (textContent) {
+        // building SVG and adding that to the DOM
+        var svg = buildSVG(viewport, textContent);
+        return svg;
+      });
+    });
+  });
+}
 
-                $scope.pdfToPlainText = function(pdfData) {
+// document.addEventListener('DOMContentLoaded', function () {
+//   if (typeof PDFJS === 'undefined') {
+//     alert('Built version of PDF.js was not found.\n' +
+//           'Please run `node make generic`.');
+//     return;
+//   }
+//   pageLoaded();
+// });
+// pageLoaded();
+
+                //$scope.pdfToPlainText = function(pdfData) {
                     var newtab = {
                         title: 'pdfData',
                         content: '<div id="pdf"></div>'
                     };
-                    $scope.phd.documents.push(newtab);
-
-                    PDFJS.disableWorker = false;
-                    $('#pdf').children().remove();
-                    var pdf = PDFJS.getDocument(pdfData);
-                    pdf.then(getPages);
-                };
+                    //$scope.phd.documents.push(newtab);
+                    PDFJS.workerSrc = '/llp_core/bower_components/pdfjs-dist/build/pdf.worker.js';
+                    //PDFJS.disableWorker = false;
+                   // $('#pdf').children().remove();
+                    var pdf = PDFJS.getDocument(PDF_PATH);
+                    pdf.then(function(pdfDocument){getPages(pdfDocument);});
+                //};
 
                 var getPages = function(pdf) {
                     for (var i = 0; i < pdf.numPages; i++) {
-                        pdf.getPage(i + 1).then(getPageText);
+                        pdf.getPage(i + 1).then(function(page){getPageText(page)});
                     }
                 };
-                var template = "</section><section class='page card card-fancy'>";
+                var template = "</pre><pre class='page card card-fancy'>";
                 var getPageText = function(page) {
-                    var sectionwrap = angular.element(template).appendTo('#pdf');
+                    var sectionwrap = angular.element(template).appendTo($element);
                     page.getTextContent().then(function(textContent) {
                         console.log(textContent);
-                        angular.forEach(textContent, function(o, key) {
+                        var section = '';
+                        angular.forEach(textContent.items, function(o, key) {
 
-                            var section = '';
-                            angular.forEach(o, function(i, key) {
-                                // $(sectionwrap).append(i.str + ' ');
-                                section = section + i.str;
-                                return section;
-                            });
-                            $(sectionwrap).append(section);
-                            $scope.pages.push(section);
+                            // var section = '';
+                            
+                            
+                            // angular.forEach(o, function(i, key) {
+                            //     // $(sectionwrap).append(i.str + ' ');
+                            //     section = section + ' ' + i.str;
+                            //     return section;
+                            // });
+                            section = section + ' ' + o.str;
+                            $element.append(pageLoaded(key))
 
                         });
-
+                            $(sectionwrap).append(section);
+                            
+                            $scope.pages.push(section);
                         // textContent.forEach(function(o) {
 
                         // });
@@ -132,15 +196,21 @@ angular.module('llp.pdf', ['LocalStorageModule'])
                 // }
 
                 //   });
-
+});
             };
 
             return {
 
                 restrict: "A",
-                controller: "PDFFilesController",
-                controllerAs: "pdff",
-                bindToController: true,
+                template: '<div ng-repeat="page in pages" ng-bind-html="page | highlight: keywords | trustAsHTML" class="card card-fancy card-block"></div>',
+                //controller: "PDFFilesController",
+                //controllerAs: "pdff",
+                //bindToController: true,
+                scope:{
+                    pdfData: '@',
+                    pages: '=',
+                    keywords: '='
+                },
                 link: linkfunction
             };
         }
