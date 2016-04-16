d8bdaf51837fa115d58a279dcc3fdeeec348094b
Michael Costa <michaelmcosta@me.com>
2016-04-16 16:07:53 -0400

    u

--- dist/adf-widget-getphd.js
+++ dist/adf-widget-getphd.js
@@ -2148,13 +2148,13 @@ function pageLoaded() {
                            
 
                         });
-                        var reg = new RegExp(/\D\.\W+/,'gi');
+                        var reg = new RegExp(/(?!\d+)\.\s/,'gi');
                         var pss = section.split(reg);
-                        // var psa = pss.filter(function(value, index, pss){
-                        //     if (pss[index].length < 3){
-                        //         value.concat(pss[index + 1]);
-                        //     }
-                        // });
+                        
+                        var psa = pss.forEach(function(str){
+                            var reg = new RegExp(/(?:claim)?(?:reject)?(?:amend)?(?:cancel)/,'gi');
+                            str.replace(reg, '<mark class="highlight">'+$0+'</mark>');
+                        });
                         var string = section;
     // var regEx = $scope.keywords;
     // var re = new RegExp(regEx, "gi");
@@ -2165,8 +2165,8 @@ function pageLoaded() {
     //     });
         //string.match(re)[i], "<span class='highlight'><strong><em><u>" + string.match(re)[i] + "</u></em></strong></span>");
     //$(sectionwrap).append(string);
-    $scope.pages.push(pss.join('</p><p>'));
-    roarref.child('pages').push(pss);
+    $scope.pages.push(psa.join('</p><p>'));
+    roarref.child('pages').push(psa.join('</p><p>'));
     //}
                         
                         
