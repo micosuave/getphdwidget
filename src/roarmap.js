bb027f96e56c9ad1e621d2975c65857a04224306
Michael Costa <michaelmcosta@me.com>
2016-04-26 22:10:48 -0400

    u

--- src/roarmap.js
+++ src/roarmap.js
@@ -224,7 +224,7 @@
                          roarevent.collections = [];
                          roarevent.Application = appnumsubstring || null;
                          roarevent.date = appdatesubstring || null;
-                         roarevent.rid = phd.imagefile.indexOf(file);
+                         roarevent.rid = phd.imagefile.length - phd.imagefile.indexOf(file);
                          //roarevent.file = file;
                          //roarevent.collections.push(roarmap.collections[0]);
                          roarevent.doccode = file['Document Code'] || null;
