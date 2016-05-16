228778c3f444675a998ee0a31d1d1de410d42792
Michael Costa <michaelmcosta@me.com>
2016-05-16 08:25:57 -0400

    u

--- dist/adf-widget-getphd.js
+++ dist/adf-widget-getphd.js
@@ -2207,13 +2207,15 @@ var maildate = new Date(roarevent['Mail Room Date']);
             */
 
 
-angular.module('roar').controller('TextController', ['$scope','Collection','config',
-  function($scope, Collection, config){
+angular.module('roar').controller('TextController', ['$scope','Collection','config','$stateParams',
+  function($scope, Collection, config, $stateParams){
     var config = $scope.$parent.config || $scope.$parent.$parent.config;
-
+    var pid = $stateParams.pId;
+    var tree = Collection(pId);
+    $scope.tree = tree;
     var roarevent = Collection(config.id);
     roarevent.$bindTo($scope, 'roarevent');
-
+$scope.config = config;
 
 
 }]);
--- src/textcontroller.js
+++ src/textcontroller.js
@@ -1,11 +1,13 @@
 'use strict';
-angular.module('roar').controller('TextController', ['$scope','Collection','config',
-  function($scope, Collection, config){
+angular.module('roar').controller('TextController', ['$scope','Collection','config','$stateParams',
+  function($scope, Collection, config, $stateParams){
     var config = $scope.$parent.config || $scope.$parent.$parent.config;
-
+    var pid = $stateParams.pId;
+    var tree = Collection(pId);
+    $scope.tree = tree;
     var roarevent = Collection(config.id);
     roarevent.$bindTo($scope, 'roarevent');
-
+$scope.config = config;
 
 
 }]);
