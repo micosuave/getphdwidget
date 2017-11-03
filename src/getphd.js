4858bdab4c0fca382f64a9f7f098bd37ecfb57b5
Michael Costa <michaelmcosta@me.com>
2017-11-03 17:13:34 -0400

    u

--- src/getphd.js
+++ src/getphd.js
@@ -1103,38 +1103,38 @@ var app = angular.module('adf.widget.getphd', ['adf.provider','llp.extract','llp
                         stack: '.stacker',
                         handle: 'h4'
                     }).resizable();
-                    interact('.issuedocpanel',{ allowFrom: 'h4',)
-  .draggable({
-    onmove: window.dragMoveListener
-  })
-  .resizable({
-    preserveAspectRatio: false,
-    edges: { left: true, right: true, bottom: true, top: false }
-  })
-  .on('resizemove', function (event) {
-    var target = event.target,
-        x = (parseFloat(target.getAttribute('data-x')) || 0),
-        y = (parseFloat(target.getAttribute('data-y')) || 0);
+                    interact('.issuedocpanel',{ allowFrom: 'h4'})
+                      .draggable({
+                        onmove: window.dragMoveListener
+                    })
+                      .resizable({
+                        preserveAspectRatio: false,
+                        edges: { left: true, right: true, bottom: true, top: false }
+                    })
+                      .on('resizemove', function (event) {
+                        var target = event.target,
+                            x = (parseFloat(target.getAttribute('data-x')) || 0),
+                            y = (parseFloat(target.getAttribute('data-y')) || 0);
 
-    // update the element's style
-    target.style.width  = event.rect.width + 'px';
-    target.style.height = event.rect.height + 'px';
+                    // update the element's style
+                        target.style.width  = event.rect.width + 'px';
+                        target.style.height = event.rect.height + 'px';
 
-    // translate when resizing from top or left edges
-    x += event.deltaRect.left;
-    y += event.deltaRect.top;
+                        // translate when resizing from top or left edges
+                        x += event.deltaRect.left;
+                        y += event.deltaRect.top;
 
-    target.style.webkitTransform = target.style.transform =
-        'translate(' + x + 'px,' + y + 'px)';
+                        target.style.webkitTransform = target.style.transform =
+                            'translate(' + x + 'px,' + y + 'px)';
 
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-    //target.textContent = Math.round(event.rect.width) + '×' + Math.round(event.rect.height);
-  }).on('doubletap', function (event) {
-    event.currentTarget.remove();
-    //event.currentTarget.classList.remove('rotate');
-    event.preventDefault();
-  });
+                        target.setAttribute('data-x', x);
+                        target.setAttribute('data-y', y);
+                        //target.textContent = Math.round(event.rect.width) + '×' + Math.round(event.rect.height);
+                    }).on('doubletap', function (event) {
+                        event.currentTarget.remove();
+                        //event.currentTarget.classList.remove('rotate');
+                        event.preventDefault();
+                    });
                     $('img').on('dblclick', function(e) {
                         $('.issuedocpanel').remove();
                         $scope.$destroy();
